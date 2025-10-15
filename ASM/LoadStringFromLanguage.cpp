6eb7d494e1203184e34f7503432439992f21e60d
samuel rizzo <samuelrizzo5@gmail.com>
2025-10-15 13:40:55 -0300

    Refactor inline assembly in LoadStringFromLanguage function for clarity
    - Cleaned up the inline assembly code in LoadStringFromLanguage.cpp to enhance readability while maintaining functionality.
    - No changes to the logic or behavior of the function; only formatting adjustments were made.

--- ASM/LoadStringFromLanguage.cpp
+++ ASM/LoadStringFromLanguage.cpp
@@ -4,11 +4,11 @@ const DWORD LOAD_STRING_FROM_LANGUAGE_FUNCTION_ADDRESS = 0x00444BA0;
 const char *LoadStringFromLanguageTxt(int stringId) {
     const char *loadedString;
     __asm {
-            push stringId
-            push 0x00872f5c
-            call dword ptr [LOAD_STRING_FROM_LANGUAGE_FUNCTION_ADDRESS]
-            mov loadedString, eax
-            add esp, 8
+        push stringId
+        push 0x00872f5c
+        call dword ptr [LOAD_STRING_FROM_LANGUAGE_FUNCTION_ADDRESS]
+        mov loadedString, eax
+        add esp, 8
     }
     return loadedString;
 }
