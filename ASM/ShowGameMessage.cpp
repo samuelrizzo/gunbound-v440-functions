0940fa1d3f9d84d7b69fe0a8567d894ba1070c32
samuel rizzo <samuelrizzo5@gmail.com>
2025-10-29 20:09:21 -0300

    Refactor ShowGameMessage implementation to use function pointers
    - Updated the ShowGameMessage function in ShowGameMessage.cpp to replace inline assembly with function pointers for invoking the native message display routines, enhancing code clarity and maintainability.
    - Adjusted the documentation in ShowGameMessage.h to reflect the changes in implementation, including updated parameter passing details and the use of function pointers.

--- ASM/ShowGameMessage.cpp
+++ ASM/ShowGameMessage.cpp
@@ -1,21 +1,19 @@
 #include "ShowGameMessage.h"
 
-const DWORD SHOW_GAME_MESSAGE_FUNCTION_ADDRESS_1 = 0x004CF180;
-const DWORD SHOW_GAME_MESSAGE_FUNCTION_ADDRESS_2 = 0x004DCB50;
+typedef void(__stdcall *ShowGameMessageFunc1)(const char *message, int color, int icon);
+typedef void(__stdcall *ShowGameMessageFunc2)(void *obj, const char *message, char color, WORD icon);
 
 void ShowGameMessage(int icon, int color, const char *message) {
-    __try {
-        __asm {
-            push icon
-            push color
-            push message
-            call SHOW_GAME_MESSAGE_FUNCTION_ADDRESS_1
+    const DWORD SHOW_GAME_MESSAGE_FUNCTION_ADDRESS_1 = 0x004CF180;
+    const DWORD SHOW_GAME_MESSAGE_FUNCTION_ADDRESS_2 = 0x004DCB50;
+    const DWORD GAME_STATE_BASE_ADDRESS = 0x008701cc;
 
-            push icon
-            push color
-            push message
-            call SHOW_GAME_MESSAGE_FUNCTION_ADDRESS_2
-        }
+    __try {
+        reinterpret_cast<ShowGameMessageFunc1>(SHOW_GAME_MESSAGE_FUNCTION_ADDRESS_1)(message, color, icon);
+
+        void *gameStateObj = reinterpret_cast<void *>(*reinterpret_cast<DWORD *>(GAME_STATE_BASE_ADDRESS));
+        reinterpret_cast<ShowGameMessageFunc2>(SHOW_GAME_MESSAGE_FUNCTION_ADDRESS_2)(
+            gameStateObj, message, static_cast<char>(color), static_cast<WORD>(icon));
     } __except (EXCEPTION_EXECUTE_HANDLER) {
     }
 }
