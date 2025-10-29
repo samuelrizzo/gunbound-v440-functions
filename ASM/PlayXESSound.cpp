314d9b8509a6176c722a8eda059ab68b0fa02311
samuel rizzo <samuelrizzo5@gmail.com>
2025-10-29 20:19:42 -0300

    Remove unnecessary whitespace in PlayXESSound.cpp

--- ASM/PlayXESSound.cpp
+++ ASM/PlayXESSound.cpp
@@ -4,6 +4,6 @@ typedef unsigned int(__cdecl *PlayXESSoundFunc)(const char *soundName, int loopF
 
 void PlayXESSound(const char *soundName) {
     const DWORD PLAY_XES_SOUND_FUNCTION_ADDRESS = 0x00529cb0;
-    
+
     reinterpret_cast<PlayXESSoundFunc>(PLAY_XES_SOUND_FUNCTION_ADDRESS)(soundName, 0);
 }
